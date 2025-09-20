181100b5a76a623552312ee1ac51d168d1d612c3
soportealephti-cpu <soportealephti@gmail.com>
2025-09-20 10:54:26 -0500

    Update api.js
    changes url api

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://api.campus.bioelectronsac.com/api",
 });
 
 // Interceptor para agregar el token a las cabeceras
